4ac9bfdf19d254746ecc4bed4d5cd33b58a3ac1b
Alvaro Jose Campos Mendez <alvaro.camposmndz@gmail.com>
2026-05-10 19:54:01 +0200

    continuing with listen and accept

--- ClientServer/server.cpp
+++ ClientServer/server.cpp
@@ -10,7 +10,7 @@ int main() {
   hints.ai_family = AF_UNSPEC;
   hints.ai_socktype = SOCK_STREAM;
   hints.ai_flags = AI_PASSIVE;
-  if ((status = getaddrinfo("172.0.0.1", "80", &hints, &servinfo) != 0)) {
+  if ((status = getaddrinfo("0.0.0.0", "4343", &hints, &servinfo) != 0)) {
     std::cout << "Addrinfo failed: " << gai_strerror(status) << std::endl;
     exit(1);
   }
@@ -22,7 +22,16 @@ int main() {
     return (1);
   }
   if (bind(sock, servinfo->ai_addr, servinfo->ai_addrlen) == -1) {
-    std::cout << "bind not possible" << std::endl;
+    std::cout << "bind not possible: " << errno << std::endl;
+    freeaddrinfo(servinfo);
+    return (1);
   }
-  // listen,then accept
+  if (listen(sock, 1) == -1) {
+    std::cout << "listen not possible: " << errno << std::endl;
+    freeaddrinfo(servinfo);
+    return (1);
+  }
+  struct addrinfo client;
+  socklen_t csize = sizeof(client);
+  int commfd = accept(sock, (struct sockaddr *)&client, &csize);
 }
