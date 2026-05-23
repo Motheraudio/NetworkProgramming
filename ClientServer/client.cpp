fc009b122c32c53eafc202e63537c7776ccd5d9e
Alvaro Jose Campos Mendez <alvaro.camposmndz@gmail.com>
2026-05-23 22:57:30 +0200

    Added rudimentary communication between server and client; missing handshake to guarantee safe comms

--- ClientServer/client.cpp
+++ ClientServer/client.cpp
@@ -1,5 +1,35 @@
 #include "client.hpp"
-int main() {
+#include "string.h"
+#include <iostream>
+#include <netdb.h>
+#include <string>
+int main()
+{
+  int status;
+  struct addrinfo *clientinfo;
+  struct addrinfo hints;
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+  if ((status = getaddrinfo("127.0.0.1", "4343", &hints, &clientinfo)) != 0)
+    return (1);
+  struct addrinfo *p = clientinfo;
   int sock;
-  struct addrinfo struc;
+  while (p != NULL)
+  {
+    sock = socket(p->ai_family, p->ai_socktype, p->ai_protocol);
+    if (sock != -1)
+      break;
+  }
+  if ((connect(sock, p->ai_addr, p->ai_addrlen) != 0))
+    return (1);
+  std::string message;
+  std::string slen;
+  while (1)
+  {
+    getline(std::cin, message);
+    slen = std::to_string(message.length());
+    send(sock, slen.c_str(), slen.length(), 0);
+    send(sock, message.c_str(), message.length(), 0);
+  }
 }
--- ClientServer/server.cpp
+++ ClientServer/server.cpp
@@ -1,8 +1,11 @@
 #include "server.hpp"
+#include <cstdlib>
+#include <iostream>
 #include <netdb.h>
 #include <string.h>
 #include <sys/socket.h>
-int main() {
+int main()
+{
   struct addrinfo hints;
   struct addrinfo *servinfo;
   int status;
@@ -10,23 +13,27 @@ int main() {
   hints.ai_family = AF_UNSPEC;
   hints.ai_socktype = SOCK_STREAM;
   hints.ai_flags = AI_PASSIVE;
-  if ((status = getaddrinfo("0.0.0.0", "4343", &hints, &servinfo) != 0)) {
+  if ((status = getaddrinfo(NULL, "4343", &hints, &servinfo) != 0))
+  {
     std::cout << "Addrinfo failed: " << gai_strerror(status) << std::endl;
     exit(1);
   }
   int sock =
       socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
-  if (sock == -1) {
+  if (sock == -1)
+  {
     std::cout << "Socket failed" << std::endl;
     freeaddrinfo(servinfo);
     return (1);
   }
-  if (bind(sock, servinfo->ai_addr, servinfo->ai_addrlen) == -1) {
+  if (bind(sock, servinfo->ai_addr, servinfo->ai_addrlen) == -1)
+  {
     std::cout << "bind not possible: " << errno << std::endl;
     freeaddrinfo(servinfo);
     return (1);
   }
-  if (listen(sock, 1) == -1) {
+  if (listen(sock, 1) == -1)
+  {
     std::cout << "listen not possible: " << errno << std::endl;
     freeaddrinfo(servinfo);
     return (1);
@@ -34,4 +41,21 @@ int main() {
   struct addrinfo client;
   socklen_t csize = sizeof(client);
   int commfd = accept(sock, (struct sockaddr *)&client, &csize);
+  std::cout << "Connection Successful" << std::endl;
+  char *buff;
+  while (1)
+  {
+    char *size = (char *)malloc(10);
+    if (!size)
+      return (1);
+    recv(commfd, size, 10, 0);
+    int isize = std::atoi(size);
+    free(size);
+    buff = (char *)malloc(isize);
+    if (!buff)
+      return (1);
+    recv(commfd, buff, isize, 0);
+    std::cout << buff << std::endl;
+    free(buff);
+  }
 }
